7bb6a508878c22c00e18b846db6df2007b307848
DoubleVVdouze <birjam.walid@gmail.com>
2019-02-01 22:05:31 +0100

    Ligue Bronze
    Ligue Bronze

--- main.cpp
+++ main.cpp
@@ -2,6 +2,23 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
+
+// Structure pour contenir des Point/Vecteur 2 dimensions.
+struct Vec2i{
+    int x;
+    int y;
+    
+    int length(){
+        return trunc(sqrt(x * x + y * y));
+    }
+};
+
+// Fonction qui retourne la distance entre 2 points.
+int distance(int firstPointX, int firstPointY, int secondPointX, int secondPointY){
+    return trunc(sqrt((firstPointX - secondPointX) * (firstPointX - secondPointX) +
+                (firstPointY - secondPointY) * (firstPointY - secondPointY)));
+}
 
 using namespace std;
 
@@ -12,6 +29,13 @@ using namespace std;
 int main()
 {
     bool boostUsed = false;
+    bool firstFrame = true;
+    
+    Vec2i speed;
+    Vec2i opponentSpeed;
+
+    Vec2i lastPosition;
+    Vec2i lastOpponentPosition;
     
     // game loop
     while (1) {
@@ -25,6 +49,21 @@ int main()
         int opponentX;
         int opponentY;
         cin >> opponentX >> opponentY; cin.ignore();
+        
+        if(firstFrame){
+            lastPosition.x = x;
+            lastPosition.y = y;
+            firstFrame = false;
+        }
+        
+        // Calcul des vitesses
+        speed.x = x - lastPosition.x;
+        speed.y = y - lastPosition.y;
+        opponentSpeed.x = opponentX - lastOpponentPosition.x;
+        opponentSpeed.y = opponentY - lastOpponentPosition.y;
+        
+        cerr << "Pod Speed : (" << speed.x << ", " << speed.y << ") : " << speed.length() << endl;
+        cerr << "Opponent Speed : (" << opponentSpeed.x << ", " << opponentSpeed.y << ") : " << opponentSpeed.length() << endl;
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
@@ -34,23 +73,49 @@ int main()
         // followed by the power (0 <= thrust <= 100)
         // i.e.: "x y thrust"
         int thrust = 0;
+        float directionCoef = 0.f;
         bool boost = false;
         
         if(nextCheckpointAngle > 90 || nextCheckpointAngle < -90)
             thrust = 0;
-        else if(nextCheckpointAngle < 5 && nextCheckpointAngle > -5 && boostUsed == false){
+        // Si le checkpoint est quasi tout droit, on boost.
+        else if(nextCheckpointAngle < 5 && nextCheckpointAngle > -5 && distance(x, y, nextCheckpointX, nextCheckpointY) > 2000 && boostUsed == false){
             boost = true;
-            boostUsed = true;
+        }
+        else{
+            // Si le checkpoint est entre "effectiveAngle" et 90°, alors on ralenti.
+            int effectiveAngle = 65;
+
+            directionCoef = std::min(((float)(90 - abs(nextCheckpointAngle))/(90 - effectiveAngle)), 1.f);
+            // directionCoef = std::max(directionCoef, 0.f);
+            thrust = directionCoef * 100;
+
+            cerr << "Direction coefficient : " << directionCoef << endl;
+            cerr << "Puissance : " << thrust << endl;
+        }
+            
+        // Si le vaisseau adverse est proche, et que notre vaisseau à suffisamment de vitesse, on va chercher
+        // à pousser le vaisseau ennemi de sa trajectoire. 
+        if(distance(x, y, opponentX, opponentY) < 1000 && speed.length() > 500){
+            cout << opponentX << " " << opponentY << " " << 100 << endl;
+            continue;
         }
         else
-            thrust = 100;
+            cout << nextCheckpointX << " " << nextCheckpointY << " ";
             
-        cout << nextCheckpointX << " " << nextCheckpointY << " ";
-        if(boost)
+        if(boost){
             cout << "BOOST";
+            boostUsed = true;
+        }
         else
             cout << thrust;
         
         cout << endl;
+        
+        lastPosition.x = x;
+        lastPosition.y = y;
+        
+        lastOpponentPosition.x = opponentX;
+        lastOpponentPosition.y = opponentY;
     }
 }
